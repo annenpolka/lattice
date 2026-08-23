25c2073dd6a8e1fefc199d26cf0df8b20131a25d
Annenpolka <56289203+annenpolka@users.noreply.github.com>
2026-08-23 16:12:15 +0900

    CHI-90: overlay align left|center|right is in-box TextNode alignment

--- crates/lattice-core/src/evaluate.rs
+++ crates/lattice-core/src/evaluate.rs
@@ -1,15 +1,1 @@
-//! Evaluate a flattened timeline at time `t` into a `RenderScene` snapshot + `AudioPlan`.
-#![allow(clippy::cast_possible_truncation)]
-
-use crate::NormalizedScale;
-use crate::ir::{PlacementKind, TimeSpan};
-use crate::locator::MediaLocator;
-use crate::overlay::{OverlayAlign, OverlayBar, OverlaySize, OverlayStyle};
-use crate::property::{Curve, Easing, Keyframe, Property};
-use crate::scene::{
-    AnimatedStyle, AssetRef, AudioClip, AudioPlan, BlendMode, Canvas, FontIdentity, FontSpec,
-    GroupNode, NodeProps, Rect, RenderNode, RenderScene, Rgba, ShapeKind, ShapeNode, TextNode,
-    Transform, VideoNode,
-};
-use crate::time::Time;
-use crate::timeline::{Timeline, TimelineClip, TimelineError};
+PLACEHOLDER_WILL_BE_TOO_LONG
